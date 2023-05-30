0b50916f98207423a1dd1e8b5246328bc1c5ea79
Alexandre Avanian <git@alexandre.avanian.net>
2023-05-30 22:09:24 +0800

    prusaslicer-beta 2.6.0-beta3

--- Casks/prusaslicer-beta.rb
+++ Casks/prusaslicer-beta.rb
@@ -1,6 +1,6 @@
 cask "prusaslicer-beta" do
-  version "2.6.0-beta2,202305170918"
-  sha256 "05a1f40997057456c1b56e87cac66941f5f6a6d37e36bd4b358a815b4022b0d9"
+  version "2.6.0-beta3,202305261411"
+  sha256 "8861fb1eb18432d11d57abde08e801529c3679ba5ffa2ca912a33258c69387c6"
 
   url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.csv.first}/PrusaSlicer-#{version.csv.first}+MacOS-universal-#{version.csv.second}.dmg",
       verified: "github.com/prusa3d/PrusaSlicer/"
