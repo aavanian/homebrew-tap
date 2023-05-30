c8f1b1e00c7c205632893ad006d397ec85f45547
Alexandre Avanian <git@alexandre.avanian.net>
2023-05-30 21:18:41 +0800

    fix PrusaSlicer beta recipe to be more generic so can use brew command to bump new version

--- Casks/prusaslicer-beta.rb
+++ Casks/prusaslicer-beta.rb
@@ -1,8 +1,8 @@
 cask "prusaslicer-beta" do
-  version "2.6.0,202305170918"
+  version "2.6.0-beta2,202305170918"
   sha256 "05a1f40997057456c1b56e87cac66941f5f6a6d37e36bd4b358a815b4022b0d9"
 
-  url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.csv.first}-beta2/PrusaSlicer-#{version.csv.first}-beta2+MacOS-universal-#{version.csv.second}.dmg",
+  url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.csv.first}/PrusaSlicer-#{version.csv.first}+MacOS-universal-#{version.csv.second}.dmg",
       verified: "github.com/prusa3d/PrusaSlicer/"
   name "PrusaSlicer Beta Release"
   desc "G-code generator for 3D printers (RepRap, Makerbot, Ultimaker etc.)"
